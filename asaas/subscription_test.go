9211496ed2305683a3f6ceb700d400860f093161
Gabriel Cataldo <gabriel.cataldo@petzpartner.com.br>
2023-11-27 09:35:46 -0300

    Developed new subscription test unit

---
+++ asaas/subscription_test.go
@@ -0,0 +1,18 @@
+package asaas
+
+import (
+	"context"
+	"github.com/GabrielHCataldo/go-asaas/internal/test"
+	"testing"
+	"time"
+)
+
+func TestSubscriptionGetAllNoContent(t *testing.T) {
+	accessToken, err := test.GetAccessTokenByEnv()
+	assertFatalErrorNonnull(t, err)
+	ctx, cancel := context.WithTimeout(context.TODO(), 10*time.Second)
+	defer cancel()
+	nSubscription := NewSubscription(SANDBOX, *accessToken)
+	resp, errAsaas := nSubscription.GetAll(ctx, GetAllSubscriptionsRequest{})
+	assertResponseNoContent(t, resp, errAsaas)
+}
